e25b8d4c3dea4a881365f974a4fa7c43eae6e1f2
Shaunythunder <shaunythunder@gmail.com>
2025-10-17 07:32:54 -0400

    1: Start of project. Blocked out necessary scenes. Will be working on scripting the main scene next.

---
+++ scripts/scenes/how_to_play.gd
@@ -0,0 +1,16 @@
+extends Control
+
+# ========== Constants ==========
+
+const MAIN_MENU_PATH: String = "res://scenes/MainMenu.tscn"
+
+# ========== Godot Runtime ==========
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# ========== Button Callbacks ==========
+
+func _on_back_to_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file(MAIN_MENU_PATH)
---
+++ scripts/scenes/main_menu.gd
@@ -0,0 +1,29 @@
+# Godot v4.5.1
+# Author: Shaunythunder
+
+extends Control
+
+# ========== Constants ==========
+
+const DUEL_SCENE_PATH: String = "res://scenes/Duel.tscn"
+const HOW_TO_PLAY_SCENE_PATH: String = "res://scenes/HowToPlay.tscn"
+
+# ========== Godot Runtime ==========
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# ========== Button Callbacks ==========
+
+## Change to Duel scene
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file(DUEL_SCENE_PATH)
+
+## Change to Duel scene
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().change_scene_to_file(HOW_TO_PLAY_SCENE_PATH)
+	
+## Quit Game
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+	
